e0b9efbfa9aed2d90ebbd3b5f002871f5ff7ede5
moefsaadi <moefsaadi@gmail.com>
2022-09-26 16:09:56 -0400

    More recycler view work, onBindViewHolder not working

--- app/src/main/java/com/example/kotlinmigration/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmigration/MainActivity.kt
@@ -4,17 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.kotlinmigration.API.PostsJsonItem
 import com.example.kotlinmigration.API.ServiceAPI
-import retrofit2.Call
-import retrofit2.Retrofit
-import retrofit2.create
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
 class MainActivity : AppCompatActivity() {
-
-    private var TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +23,42 @@ class MainActivity : AppCompatActivity() {
         val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addCallAdapterFactory()//TODO implement gson mf
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
+            .create(ServiceAPI::class.java)
 
 
-        val serviceAPI = retrofit.create(ServiceAPI::class.java)
-        val call = serviceAPI.getPosts()
-
-
-
-
-
+        //might not need this stuff down here after implementing recycler view
+        GlobalScope.launch(Dispatchers.IO){
+            val response = retrofit.getPosts().awaitResponse()
+            if(response.isSuccessful){
+                val postList = response.body()
+                val adapter = MyAdapter
 
 
     }
+
+//    fun getData(){
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//            .create(ServiceAPI::class.java)
+//
+//
+//        //might not need this stuff down here after implementing recycler view
+//        GlobalScope.launch(Dispatchers.IO){
+//            val response = retrofit.getPosts().awaitResponse()
+//            if(response.isSuccessful){
+//                val postList = response.body()
+//                val adapter = RecyclerViewPostAdapter(postList, MainActivity.this)
+//
+//
+//
+//            }
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/kotlinmigration/MyAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.kotlinmigration
+
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import com.example.kotlinmigration.API.PostsJsonItem
+
+class MyAdapter() : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    private val myList = emptyList<PostsJsonItem>()
+
+    inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+       //return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.single_post,parent,false))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_post,parent,false)
+        return MyViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int {
+        return myList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.itemView.
+
+
+
+    }
+
+}
+
