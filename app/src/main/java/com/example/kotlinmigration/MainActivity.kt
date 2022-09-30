9c84357eb8ba4738dccde5e2ab7b76444a510552
moefsaadi <moefsaadi@gmail.com>
2022-09-30 17:07:01 -0400

    button not working

--- app/src/main/java/com/example/kotlinmigration/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmigration/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinmigration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -22,35 +23,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myButton: Button = findViewById(R.id.button)
         var list = listOf<PostsJsonItem>()
 
 
-        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        myButton.setOnClickListener {
+            val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+            recyclerView.layoutManager = LinearLayoutManager(this)
 
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
 
 
-        val serviceApi = retrofit.create(ServiceAPI::class.java)
+            val serviceApi = retrofit.create(ServiceAPI::class.java)
 
-        val call = serviceApi.getPosts()
-        call.enqueue(object: Callback<List<PostsJsonItem>>{
-            override fun onResponse(call: Call<List<PostsJsonItem>>, response: Response<List<PostsJsonItem>>)
-            {
-                if(!response.isSuccessful){ return }
+            val call = serviceApi.getPosts()
+            call.enqueue(object : Callback<List<PostsJsonItem>> {
+                override fun onResponse(
+                    call: Call<List<PostsJsonItem>>,
+                    response: Response<List<PostsJsonItem>>
+                ) {
+                    if (!response.isSuccessful) {
+                        return
+                    }
 
-                recyclerView.adapter = MyAdapter(response.body()!!)
-            }
+                    recyclerView.adapter = MyAdapter(response.body()!!)
+                }
 
-            override fun onFailure(call: Call<List<PostsJsonItem>>, t: Throwable)
-            {
-                Toast.makeText(applicationContext,"Failure",Toast.LENGTH_SHORT).show()
-            }
-        })
+                override fun onFailure(call: Call<List<PostsJsonItem>>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Failure", Toast.LENGTH_SHORT).show()
+                }
+            })
 
+        }
     }
 }
